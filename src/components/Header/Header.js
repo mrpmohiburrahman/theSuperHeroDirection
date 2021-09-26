eb524bf4051ac80cf2dbdc2fa59405e37630aa86
mrpmohiburrahman <mrpmohiburrahman@gmail.com>
2021-09-26 19:38:10 +0600

    change header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="" />
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
-            </nav>
+            <h1>Developer Conference</h1>
+            <h3>Total Budget: 500000$</h3>
         </div>
     );
 };
