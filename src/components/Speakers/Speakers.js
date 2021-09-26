9ff4704656ebcaeff91a1c88e0bebf7969be0478
mrpmohiburrahman <mrpmohiburrahman@gmail.com>
2021-09-26 21:05:44 +0600

    fix total summary

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -2,32 +2,19 @@ import React from 'react';
 import './Speakers.css';
 
 const Speakers = ({ cart }) => {
-    // const { cart } = props;
 
-    // const totalReducer = (previous, product) => previous + product.price;
-    // const total = cart.reduce(totalReducer, 0);
     let totalQuantity = 0;
     let total = 0;
-    for (const product of cart) {
-        if (!product.quantity) {
-            product.quantity = 1;
-        }
-        total = total + product.price * product.quantity;
-        totalQuantity = totalQuantity + product.quantity;
+    for (const speaker of cart) {
+        if (!speaker.quantity) speaker.quantity = 1;
+        total = total + speaker.salary * speaker.quantity;
+        totalQuantity = totalQuantity + speaker.quantity;
     }
-
-    const shipping = total > 0 ? 15 : 0;
-    const tax = (total + shipping) * 10;
-    const grandTotal = total + shipping + tax;
     return (
         <div>
-            <h3>Order Summary</h3>
-            <h5>Items Ordered: {totalQuantity}</h5>
-            <br />
-            <p>Total: {total.toFixed(2)}</p>
-            <p>Shipping: {shipping}</p>
-            <p>tax: {tax.toFixed(2)}</p>
-            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+            <h3>List Summary</h3>
+            <h5>Numebr of Speakers: {totalQuantity}</h5>
+            <p>Total Cost: {total} $</p>
         </div>
     );
 };
